25fd3a6e4dbae4adbf34be755b932d77f6a72ad2
PartyCkat <Ckat@teknik.io>
2016-06-20 00:49:21 +0200

    initial comit

---
+++ plugins/README.md
@@ -0,0 +1,7 @@
+# Plugins:
+
+## No Emoji
+
+Replaces emoji pics with whatever is in their alt attribute,  most of the time the symbol version of emoji.
+
+![NoEmoji](https://ckat.space/ship/FB5.png)
---
+++ plugins/noEmoji.plugin.js
@@ -0,0 +1,40 @@
+//META{"name":"noEmoji"}*//
+var noEmoji = function(){};
+noEmoji.prototype.convert = function(){
+    $(".message img").each(function(){
+        var text = $(this);
+        // identify emojis by lack of href
+        if(text.attr("href") != undefined)
+            return true
+        else 
+            text.replaceWith(text.attr("alt"))
+    });
+};
+
+noEmoji.prototype.onMessage = function(){
+    setTimeout(this.convert(), 2000);
+};
+noEmoji.prototype.onSwitch = function(){
+    this.convert();
+};
+noEmoji.prototype.start = function(){
+    this.convert();
+};
+noEmoji.prototype.load = function(){};
+noEmoji.prototype.unload = function(){};
+noEmoji.prototype.stop = function(){};
+noEmoji.prototype.getSettingsPanel = function(){
+    return "";
+};
+noEmoji.prototype.getName = function(){
+    return "No Emoji";
+};
+noEmoji.prototype.getDescription = function(){
+    return "Replaces emoji pics with whatever is in their alt attribute,  most of the time the symbol version of emoji.";
+};
+noEmoji.prototype.getVersion = function(){
+    return "0.1";
+};
+noEmoji.prototype.getAuthor = function(){
+    return "Catblaster/Ckath";
+};
