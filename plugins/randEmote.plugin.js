c6b8e42787cb5815af7054fcaf8ebea264487aad
PartyCkat <Ckat@teknik.io>
2016-08-16 20:46:43 +0200

    added /emotem for random modifiers

--- plugins/randEmote.plugin.js
+++ plugins/randEmote.plugin.js
@@ -1,6 +1,7 @@
 //META{"name":"randEmote"}*//
 var randEmote = function () {};
 var emoteArray = [];
+var eModifiers = [':shake', ':shake2', ':shake3', ':spin', ':spin2', ':spin3', ':1spin', ':2spin', ':3spin', ':pulse'];
 randEmote.prototype.start = function () {
     this.attachParser();
     for(var k in emotesTwitch['emotes']) emoteArray.push(k);
@@ -21,24 +22,39 @@ randEmote.prototype.attachParser = function(){
         if(code !== 13) return;
         var text;
 	try{var val = $('.channel-textarea textarea').val();
-        if(val.startsWith('/emote')){
+    if(val.startsWith('/emotem')){
 	    var arg = val.split(' ');
 	    if(arg[1] == undefined || isNaN(arg[1]))
-		randEmote.sendMsg(emoteArray[Math.floor(Math.random()*emoteArray.length)]);
+		    randEmote.sendMsg(emoteArray[Math.floor(Math.random()*emoteArray.length)] + eModifiers[Math.floor(Math.random()*eModifiers.length)]);
 	    else
-		randEmote.sendMsg(randEmote.getEmote(arg[1]));
+		    randEmote.sendMsg(randEmote.getEmote(arg[1], true));
 	    $(this).val("");
 	    e.preventDefault();
 	    e.stopPropagation();
 	    return;
 	}
+    else if(val.startsWith('/emote')){
+	    var arg = val.split(' ');
+	    if(arg[1] == undefined || isNaN(arg[1]))
+		    randEmote.sendMsg(emoteArray[Math.floor(Math.random()*emoteArray.length)]);
+	    else
+		    randEmote.sendMsg(randEmote.getEmote(arg[1], false));
+	    $(this).val("");
+	    e.preventDefault();
+	    e.stopPropagation();
+	    return;
+	}
+    
 	}catch(e){ console.warn("randEmote: unable to attach to textarea: " + e); }
     };
     el[0].addEventListener("keydown", this.handleKeypress, false);
 };
-randEmote.getEmote = function(n){
+randEmote.getEmote = function(n, modifiers){
     var emoteStr = "";
-    for(var i=0; i<n; i++) emoteStr += emoteArray[Math.floor(Math.random()*emoteArray.length)] + ' ';
+    if(modifiers)
+        for(var i=0; i<n; i++) emoteStr += emoteArray[Math.floor(Math.random()*emoteArray.length)] + eModifiers[Math.floor(Math.random()*eModifiers.length)] + ' ';
+    else
+        for(var i=0; i<n; i++) emoteStr += emoteArray[Math.floor(Math.random()*emoteArray.length)] + ' ';
     return emoteStr;
 };
 randEmote.sendMsg = function(text) {
@@ -68,10 +84,10 @@ randEmote.prototype.getName = function () {
     return "Random emotes";
 };
 randEmote.prototype.getDescription = function () {
-    return "Send one or more random emotes with /emote <amount>";
+    return "Send one ore more random emotes with /emote <amount>, /emotem for random modifiers";
 };
 randEmote.prototype.getVersion = function () {
-    return ".2";
+    return ".3";
 };
 randEmote.prototype.getAuthor = function () {
     return "Ckat/Catblaster";
