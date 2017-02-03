0b97b2905c24e871d32056141f11f0b2981deef1
Ckat <Ckat@teknik.io>
2017-02-03 01:36:17 +0100

    cleanup tape.sh

--- scripts/tape.sh
+++ scripts/tape.sh
@@ -2,7 +2,7 @@
 # simple script to install bd
 # first argument should be discord directory
 # deps: nodejs asar(install through npm install -g asar), sed, wget, unzip
-# (c) Ckat 2017-02-03
+# Ckat 2017-02-03
 
 ROOT_UID="0"
 [ -z "$1" ] && echo "error: No discord directory supplied" && exit 1
@@ -21,13 +21,13 @@ echo "=> removing old app folder"
 sudo rm -rf "$1/resources/app"
 
 echo "=> unpacking discord asar"
-sudo asar e "$1/resources/app.asar" "$1/resources/app"
+asar e "$1/resources/app.asar" "$1/resources/app"
 
 echo "=> preparing discord files"
-sudo sed "/_fs2/ a var _betterDiscord = require('betterdiscord'); var _betterDiscord2;" "$1/resources/app/index.js" > /tmp/bd/index.js
-sudo mv /tmp/bd/index.js "$1/resources/app/index.js"
-sudo sed "/mainWindow = new/ a _betterDiscord2 = new _betterDiscord.BetterDiscord(mainWindow);" "$1/resources/app/index.js" > /tmp/bd/index.js
-sudo mv /tmp/bd/index.js "$1/resources/app/index.js"
+sed "/_fs2/ a var _betterDiscord = require('betterdiscord'); var _betterDiscord2;" "$1/resources/app/index.js" > /tmp/bd/index.js
+mv /tmp/bd/index.js "$1/resources/app/index.js"
+sed "/mainWindow = new/ a _betterDiscord2 = new _betterDiscord.BetterDiscord(mainWindow);" "$1/resources/app/index.js" > /tmp/bd/index.js
+mv /tmp/bd/index.js "$1/resources/app/index.js"
 
 echo "=> finishing up"
-sudo mv /tmp/bd "$1/resources/app/node_modules/betterdiscord"
+mv /tmp/bd "$1/resources/app/node_modules/betterdiscord"
