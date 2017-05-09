d1b297b8fd2658fe9c596e11e57cc3ad3f7c48ae
Ckat <ckat@teknik.io>
2017-05-09 21:45:45 +0200

    install from master, minor cleanup, unzip is no longer a dep

--- scripts/README.md
+++ scripts/README.md
@@ -6,7 +6,7 @@ A very basic install script to install bd, should get the job done most of the t
 
 usage: tape.sh (give directory or fill in later)
 
-deps: nodejs (for asar install), wget, unzip, a unix based system
+deps: nodejs (for asar install), wget, linux
 
 for a more featureful implementation which can also install discord and beautiful discord check out [discorddownloader](https://github.com/simoniz0r/discorddownloader)
 
--- scripts/tape.sh
+++ scripts/tape.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # simple script to install bd
 # first argument can be discord directory
-# deps: nodejs (for asar install), wget, unzip
+# deps: nodejs (for asar install), wget
 # (c) Ckat 2017-02-03
 # Changes by simonizor 2017-02-04
+# Last updated: 2017-05-09 (Ckat)
 
 if [[ -z "$1" ]]; then
     echo -n "Input the directory you would like to install BetterDiscord to and press [ENTER]"
@@ -23,13 +24,11 @@ echo "Checking deps..."
 npmIsInstalled=1
 nodejsIsInstalled=1
 wgetIsInstalled=1
-unzipIsInstalled=1
 asarIsInstalled=1
 asarIsInstalledCorrectly=1
 type npm >/dev/null 2>&1 || { npmIsInstalled=0; }
 type node >/dev/null 2>&1 || { nodejsIsInstalled=0; }
 type wget >/dev/null 2>&1 || { wgetIsInstalled=0; }
-type unzip >/dev/null 2>&1 || { unzipIsInstalled=0; }
 type asar >/dev/null 2>&1 || { asarIsInstalled=0; }
 
 if [ "$npmIsInstalled" = "0" ]; then
@@ -42,10 +41,6 @@ if [ "$nodejsIsInstalled" = "0" ]; then
 fi
 if [ "$wgetIsInstalled" = "0" ]; then
     echo "wget not found, unable to continue"
-    exit 1
-fi
-if [ "$unzipIsInstalled" = "0" ]; then
-    echo "unzip not found, unable to continue"
     exit 1
 fi
 if [ "$asarIsInstalled" = "0" ]; then
@@ -69,20 +64,6 @@ killall -SIGKILL Discord >/dev/null 2>&1 || { discordRunning=0; }
 killall -SIGKILL DiscordCanary >/dev/null 2>&1 || { discordCanaryRunning=0; }
 killall -SIGKILL DiscordPTB >/dev/null 2>&1 || { discordPTBRunning=0; }
 
-echo "Cleaning old install..."
-sudo rm /tmp/bd.zip
-sudo rm -rf /tmp/bd
-
-echo "Downloading BetterDiscord..."
-wget -O /tmp/bd.zip https://github.com/Jiiks/BetterDiscordApp/archive/stable16.zip
-
-echo "Preparing BetterDiscord files..."
-unzip /tmp/bd.zip 
-sudo mv ./BetterDiscordApp-stable16 /tmp/bd
-sudo mv /tmp/bd/lib/Utils.js /tmp/bd/lib/utils.js
-sed -i "s/'\/var\/local'/process.env.HOME + '\/.config'/g" /tmp/bd/lib/BetterDiscord.js
-sed -i "s/bdstorage/bdStorage/g" /tmp/bd/lib/BetterDiscord.js
-
 echo "Removing app folder from Discord directory..."
 sudo rm -rf "$DIR/resources/app"
 
@@ -90,13 +71,20 @@ echo "Unpacking Discord asar..."
 sudo asar e "$DIR/resources/app.asar" "$DIR/resources/app"
 
 echo "Preparing Discord files..."
-sed "/_fs2/ a var _betterDiscord = require('betterdiscord'); var _betterDiscord2;" "$DIR/resources/app/index.js" > /tmp/bd/index.js
-sudo mv /tmp/bd/index.js "$DIR/resources/app/index.js"
-sed "/mainWindow = new/ a _betterDiscord2 = new _betterDiscord.BetterDiscord(mainWindow);" "$DIR/resources/app/index.js" > /tmp/bd/index.js
-sudo mv /tmp/bd/index.js "$DIR/resources/app/index.js"
+sed "/_fs2/ a var _betterDiscord = require('betterdiscord'); var _betterDiscord2;" "$DIR/resources/app/index.js" > /tmp/bdindex.js
+sudo mv /tmp/bdindex.js "$DIR/resources/app/index.js"
+sed "/mainWindow = new/ a _betterDiscord2 = new _betterDiscord.BetterDiscord(mainWindow);" "$DIR/resources/app/index.js" > /tmp/bdindex.js
+sudo mv /tmp/bdindex.js "$DIR/resources/app/index.js"
+sudo mkdir -p "$DIR/resources/app/node_modules/betterdiscord/lib"
 
-echo "Finishing up..."
-sudo mv /tmp/bd "$DIR/resources/app/node_modules/betterdiscord"
+echo "Downloading BetterDiscord files..."
+sudo wget -O "$DIR/resources/app/node_modules/betterdiscord/betterdiscord.js" https://raw.githubusercontent.com/Jiiks/BetterDiscordApp/master/betterdiscord.js
+sudo wget -O "$DIR/resources/app/node_modules/betterdiscord/package.json" https://raw.githubusercontent.com/Jiiks/BetterDiscordApp/master/package.json
+sudo wget -O "$DIR/resources/app/node_modules/betterdiscord/lib/BetterDiscord.js" https://raw.githubusercontent.com/Jiiks/BetterDiscordApp/master/lib/BetterDiscord.js
+sudo wget -O "$DIR/resources/app/node_modules/betterdiscord/lib/Utils.js" https://raw.githubusercontent.com/Jiiks/BetterDiscordApp/master/lib/Utils.js
+sudo wget -O "$DIR/resources/app/node_modules/betterdiscord/lib/config.json" https://raw.githubusercontent.com/Jiiks/BetterDiscordApp/master/lib/config.json
+
+echo "Instalation done, restarting any closed clients..."
 
 if [ "$discordRunning" = "1" ]; then
     echo "Starting discord..."
