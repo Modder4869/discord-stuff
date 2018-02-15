15b348c554e1a9674134448e4bb6f2469c4b3bd0
Ckath <ckat@teknik.io>
2018-02-15 17:01:42 +0100

    allow use of fixsize without restoring windowsize on start

--- plugins/fixsize.plugin.js
+++ plugins/fixsize.plugin.js
@@ -3,13 +3,19 @@ var fixsize = function () {};
 fixsize.updateSize = function () {
     bdPluginStorage.set('fixsize', 'size', require('electron').remote.getCurrentWindow().getSize());
 };
+fixsize.resetSize = function () {
+    bdPluginStorage.set('fixsize', 'size', [0, 0]);
+};
 fixsize.prototype.start = function () {
     var size = bdPluginStorage.get('fixsize', 'size');
     require('electron').remote.getCurrentWindow().setMinimumSize(1, 1);
     
-    if (size != null) {
+    if (size == null) {
+        console.log('fixsize: no config detected, creating empty config');
+        fixsize.resetSize();
+    } else if (size[0] != 0 && size[1] != 0) {
         require('electron').remote.getCurrentWindow().setSize(size[0], size[1]);
-    } else { console.error("setsize: no config found!"); }
+    }
 };
 fixsize.prototype.onSwitch = function () {};
 fixsize.prototype.load = function () {};
@@ -18,7 +24,8 @@ fixsize.prototype.stop = function () {};
 fixsize.prototype.onMessage = function () {};
 fixsize.prototype.observer = function (e) {};
 fixsize.prototype.getSettingsPanel = function () {
-	return '<button onclick="fixsize.updateSize()">save this size</button>';
+	return '<button onclick="fixsize.updateSize()">save this size</button>' +
+        '<br/><button onclick="fixsize.resetSize()">reset</button>';
 };
 fixsize.prototype.getName = function () {
     return "fix size";
@@ -27,7 +34,7 @@ fixsize.prototype.getDescription = function () {
     return "for some reason discord has a minimal window size, thats still too big. fix it with this";
 };
 fixsize.prototype.getVersion = function () {
-    return ".1";
+    return ".2";
 };
 fixsize.prototype.getAuthor = function () {
     return "ckat/cate";
